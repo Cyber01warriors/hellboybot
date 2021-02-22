02e97095c3ea864b5e1b652d8e8f062d62efca22
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-22 22:29:49 +0530

    Update fun.py

--- Mizuhararobot/modules/fun.py
+++ Mizuhararobot/modules/fun.py
@@ -268,7 +268,7 @@ dispatcher.add_handler(RLG_HANDLER)
 dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "Fun"
+__mod_name__ = "Fun 😜"
 __command_list__ = [
     "runs", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide",
     "table", "pat", "sanitize", "shout", "weebify"
