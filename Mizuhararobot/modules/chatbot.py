8e59c8546e5f85399beeb37dc60e6a1620ccb4e8
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-22 22:22:22 +0530

    Update chatbot.py

--- Mizuhararobot/modules/chatbot.py
+++ Mizuhararobot/modules/chatbot.py
@@ -156,7 +156,7 @@ dispatcher.add_handler(REMOVE_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
 dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
 
-__mod_name__ = "Chatbot"
+__mod_name__ = "Chatbot 🙋‍♀"
 __command_list__ = ["addchat", "rmchat", "listaichats"]
 __handlers__ = [
     ADD_CHAT_HANDLER, REMOVE_CHAT_HANDLER, CHATBOT_HANDLER,
