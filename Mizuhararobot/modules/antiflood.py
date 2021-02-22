6c0c38b6361922a10e6043d08cb4e47944a9c5ac
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-22 22:07:43 +0530

    Update antiflood.py

--- Mizuhararobot/modules/antiflood.py
+++ Mizuhararobot/modules/antiflood.py
@@ -355,7 +355,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Anti-Flood"
+__mod_name__ = "Anti-Flood 🚫"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood)
