e170983ab81a2549972e5fa3c066d8ba6f705adc
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-22 23:38:23 +0530

    Update truth_and_dare.py

--- Mizuhararobot/modules/truth_and_dare.py
+++ Mizuhararobot/modules/truth_and_dare.py
@@ -16,10 +16,6 @@ def dare(update: Update, context: CallbackContext):
     args = context.args
     update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
 
-__help__ = """
- • `/truth`*:* for random truth
- • `/dare`*:* for random dare
-"""
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
@@ -28,4 +24,3 @@ DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 
-__mod_name__ = "Truth or Dare 🧐"
