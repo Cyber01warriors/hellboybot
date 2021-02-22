b982cdc3a03174fbbe1d374d9862712717b52e3e
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-22 22:24:39 +0530

    Update cust_filters.py

--- Mizuhararobot/modules/cust_filters.py
+++ Mizuhararobot/modules/cust_filters.py
@@ -612,7 +612,7 @@ Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "Filters 📃"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
