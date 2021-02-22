516caa89d86781b2031bd08fcdf99091c4697eb8
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 00:05:44 +0530

    Update __main__.py

--- Mizuhararobot/__main__.py
+++ Mizuhararobot/__main__.py
@@ -50,7 +50,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = "Hi {}, my name is {}!\n\nI'm a next gen powerful group manager bot. Made by [Janindu](t.me/imjanindu).\n\nHit /help to find my list of available commands."
+PM_START_TEXT = "Hi {}, my name is {}.\n\nI'm a next gen powerful group manager bot. Made by [Janindu](t.me/imjanindu).\n\nHit /help to find my list of available commands."
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
