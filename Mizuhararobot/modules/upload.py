f73e15969434ef59ccfb98daa9356bd679159351
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 17:24:08 +0530

    Update upload.py

--- Mizuhararobot/modules/upload.py
+++ Mizuhararobot/modules/upload.py
@@ -9,6 +9,10 @@ import traceback
 from Mizuhararobot import telethn as bot
 from Mizuhararobot.modules.urluploader import download_file
 
+DOWNLOADPATH = "./Downloads/"
+    if not os.path.isdir(DOWNLOADPATH):
+        os.makedirs(DOWNLOADPATH)
+
 def get_date_in_two_weeks():
     """
     get maximum date of storage for file
@@ -104,6 +108,41 @@ async def tmp(event):
             traceback.print_exc()
             print(e)
             await event.respond(f"Uploading Failed\n\n**Error:** {e}")
+
+    raise events.StopPropagation
+
+@bot.on(events.NewMessage(pattern='/up'))
+async def up(event):
+    if event.reply_to_msg_id:
+        start = time.time()
+        url = await event.get_reply_message()
+        ilk = await event.respond("Downloading...")
+        
+        try:
+            filename = os.path.join(DOWNLOADPATH, os.path.basename(url.text))
+            file_path = await download_file(url.text, filename, ilk, start, bot)
+        except Exception as e:
+            print(e)
+            await event.respond(f"Downloading Failed\n\n**Error:** {e}")
+        
+        await ilk.delete()
+
+        try:
+            orta = await event.respond("Uploading to Telegram...")
+
+            dosya = await bot.upload_file(filename, progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, orta, start, "Uploading to Telegram...")
+                ))
+
+            zaman = str(time.time() - start)
+            await bot.send_file(event.chat.id, dosya, force_document=True, caption=f"Uploaded By {USERNAME}")
+        except Exception as e:
+            traceback.print_exc()
+
+            print(e)
+            await event.respond(f"Uploading Failed\n\n**Error:** {e}")
+        
+        await orta.delete()
 
     raise events.StopPropagation
 
