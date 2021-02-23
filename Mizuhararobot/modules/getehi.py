57402159eb2044925aae9997ec5258a8d406c7c0
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 07:31:36 +0530

    Update getehi.py

--- Mizuhararobot/modules/getehi.py
+++ Mizuhararobot/modules/getehi.py
@@ -1,7 +1,8 @@
 import html
 import random
 import time
-
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
+                      Update)
 import Mizuhararobot.modules.fun_strings as fun_strings
 from Mizuhararobot import dispatcher
 from Mizuhararobot.modules.disable import DisableAbleCommandHandler
@@ -11,7 +12,15 @@ from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async
 
-EHI_STRINGS = ("Here, latest ehi files by @TheMizukiBot.\n\n🔗 https://www.mediafire.com/folder/1g0te1sz25qrt/Mizuki+EHI\n\nReport errors to @JasonPMsBot. Share and rent me in your groups free 😌")
+EHI_STRINGS = ("Here, latest ehi files by @TheMizukiBot 👸",
+                parse_mode=ParseMode.MARKDOWN,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(
+                    [[
+                        InlineKeyboardButton(
+                            text="👉 Download EHI Files 👈",
+                            url="https://www.mediafire.com/folder/1g0te1sz25qrt/Mizuki+EHI")
+                    ]]))
 
 
 @run_async
