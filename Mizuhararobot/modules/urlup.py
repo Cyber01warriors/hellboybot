7350e54de3b49e0f1199a415f96646ffec96bd22
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 14:27:39 +0530

    Update urlup.py

--- Mizuhararobot/modules/urlup.py
+++ Mizuhararobot/modules/urlup.py
@@ -54,7 +54,7 @@ async def up(event):
 
 
 __help__ = """
- ⦁ `/up`*:* send a direct download link to group or bot pm. Then reply to that link with `/up` command to upload link on telegram
+ ⦁ `/up`*:* send a direct download link to group or bot pm. Then reply to that link with `/up` command to upload link on telegram.
 """
 
 __mod_name__ = "URL Upload 📤"
