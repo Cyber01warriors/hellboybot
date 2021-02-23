5c420e41e88468ce248da011ade253c91f21cc3c
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 19:54:27 +0530

    Update rules.py

--- Mizuhararobot/modules/rules.py
+++ Mizuhararobot/modules/rules.py
@@ -119,7 +119,7 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/rules`*:* get the rules for this chat.
+  • `/rules`*:* get the rules for this chat.
 
 *Admins only:*
  • `/setrules <your rules here>`*:* set the rules for this chat.
