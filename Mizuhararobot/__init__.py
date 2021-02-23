0aaf4f646d9aa908c46d5ea864ee2804c5c6b67d
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 06:43:42 +0530

    Update __init__.py

--- Mizuhararobot/__init__.py
+++ Mizuhararobot/__init__.py
@@ -175,8 +175,8 @@ else:
     sw = spamwatch.Client(SPAMWATCH_API)
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("saitama", API_ID, API_HASH)
-pbot = Client("Mizuhararobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+telethn = TelegramClient("mizuhara", API_ID, API_HASH)
+pbot = Client("mizuharaPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
