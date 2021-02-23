2498141fd9ae2d6e81ff2944fc09821f2dcd13db
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 14:16:37 +0530

    Update urlup.py

--- Mizuhararobot/modules/urlup.py
+++ Mizuhararobot/modules/urlup.py
@@ -49,3 +49,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+__help__ = """
+ ⦁ `/up`*:* send a direct download link to group or bot pm. Then reply to that link with /up command to upload link on telegram
+"""
+
+__mod_name__ = "URL Upload 📤"
