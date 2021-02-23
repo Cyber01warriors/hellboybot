f97ad321d889a1ee2e7777fdc4ce489b1ba54b97
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 15:33:00 +0530

    Delete urlup.py

--- Mizuhararobot/modules/urlup.py
+++
@@ -1,7 +0,0 @@
-#hmm
-
-__help__ = """
- ⦁ `/up`*:* send a direct download link to group or bot pm. Then reply to that link with `/up` command to upload link on telegram.
-"""
-
-__mod_name__ = "URL Upload 📤"
