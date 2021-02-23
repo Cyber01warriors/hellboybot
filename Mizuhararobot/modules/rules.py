5beac0ed708dc125df40e6f09334866c26a325a6
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 20:30:30 +0530

    Update rules.py

--- Mizuhararobot/modules/rules.py
+++ Mizuhararobot/modules/rules.py
@@ -1,150 +1,86 @@
-from typing import Optional
+from telethon import *
+from telethon.tl import *
 
-import Mizuhararobot.modules.sql.rules_sql as sql
-from Mizuhararobot import dispatcher
-from Mizuhararobot.modules.helper_funcs.chat_status import user_admin
-from Mizuhararobot.modules.helper_funcs.string_handling import markdown_parser
-from telegram import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-    ParseMode,
-    Update,
-    User,
-)
-from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
-from telegram.utils.helpers import escape_markdown
+import Mizuhararobot.modules.sql.rules2_sql as sql
+from Mizuhararobot import *
+from Mizuhararobot import telethn as tbot
 
 
-@run_async
-def get_rules(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    send_rules(update, chat_id)
+async def can_change_info(message):
+    result = await tbot(
+        functions.channels.GetParticipantRequest(
+            channel=message.chat_id,
+            user_id=message.sender_id,
+        )
+    )
+    p = result.participant
+    return isinstance(p, types.ChannelParticipantCreator) or (
+        isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.change_info
+    )
 
 
-# Do not async - not from a handler
-def send_rules(update, chat_id, from_pm=False):
-    bot = dispatcher.bot
-    user = update.effective_user  # type: Optional[User]
-    reply_msg = update.message.reply_to_message
-    try:
-        chat = bot.get_chat(chat_id)
-    except BadRequest as excp:
-        if excp.message == "Chat not found" and from_pm:
-            bot.send_message(
-                user.id,
-                "The rules shortcut for this chat hasn't been set properly! Ask admins to "
-                "fix this.\nMaybe they forgot the hyphen in ID",
-            )
-            return
-        else:
-            raise
-
+@register(pattern="^/rules$")
+async def _(event):
+    if event.is_private:
+        return
+    chat_id = event.chat_id
+    sender = event.sender_id
     rules = sql.get_rules(chat_id)
-    text = f"The rules for *{escape_markdown(chat.title)}* are:\n\n{rules}"
-
-    if from_pm and rules:
-        bot.send_message(
-            user.id, text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-        )
-    elif from_pm:
-        bot.send_message(
-            user.id,
-            "The group admins haven't set any rules for this chat yet. "
-            "This probably doesn't mean it's lawless though...!",
-        )
-    elif rules and reply_msg:
-        reply_msg.reply_text(
-            "Please click the button below to see the rules.",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}"
-                        )
-                    ]
-                ]
-            ),
-        )
-    elif rules:
-        update.effective_message.reply_text(
-            "Please click the button below to see the rules.",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}"
-                        )
-                    ]
-                ]
-            ),
+    if rules:
+        await event.reply(
+            "Click on the below button to get this group's rules 👇",
+            buttons=[[Button.inline("Rules", data=f"start-rules-{sender}")]],
         )
     else:
-        update.effective_message.reply_text(
+        await event.reply(
             "The group admins haven't set any rules for this chat yet. "
             "This probably doesn't mean it's lawless though...!"
         )
 
 
-@run_async
-@user_admin
-def set_rules(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    msg = update.effective_message  # type: Optional[Message]
-    raw_text = msg.text
-    args = raw_text.split(None, 1)  # use python's maxsplit to separate cmd and args
-    if len(args) == 2:
-        txt = args[1]
-        offset = len(txt) - len(raw_text)  # set correct offset relative to command
-        markdown_rules = markdown_parser(
-            txt, entities=msg.parse_entities(), offset=offset
+@tbot.on(events.CallbackQuery(pattern=r"start-rules-(\d+)"))
+async def rm_warn(event):
+    rules = sql.get_rules(event.chat_id)
+    # print(rules)
+    user_id = int(event.pattern_match.group(1))
+    if not event.sender_id == user_id:
+        await event.answer("You haven't send that command !")
+        return
+    text = f"The rules for **{event.chat.title}** are:\n\n{rules}"
+    try:
+        await tbot.send_message(
+            user_id, text, parse_mode="markdown", link_preview=False
+        )
+    except Exception:
+        await event.answer(
+            "I can't send you the rules as you haven't started me in PM, first start me !",
+            alert=True,
         )
 
-        sql.set_rules(chat_id, markdown_rules)
-        update.effective_message.reply_text("Successfully set rules for this group.")
+
+@register(pattern="^/setrules")
+async def _(event):
+    if event.is_group:
+        if not await can_change_info(message=event):
+            return
+    else:
+        return
+    chat_id = event.chat_id
+    raw_text = event.text
+    args = raw_text.split(None, 1)
+    if len(args) == 2:
+        txt = args[1]
+        sql.set_rules(chat_id, txt)
+        await event.reply("Successfully set rules for this group.")
 
 
-@run_async
-@user_admin
-def clear_rules(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
+@register(pattern="^/clearrules$")
+async def _(event):
+    if event.is_group:
+        if not await can_change_info(message=event):
+            return
+    else:
+        return
+    chat_id = event.chat_id
     sql.set_rules(chat_id, "")
-    update.effective_message.reply_text("Successfully cleared rules!")
-
-
-def __stats__():
-    return f"• {sql.num_chats()} chats have rules set."
-
-
-def __import_data__(chat_id, data):
-    # set chat rules
-    rules = data.get("info", {}).get("rules", "")
-    sql.set_rules(chat_id, rules)
-
-
-def __migrate__(old_chat_id, new_chat_id):
-    sql.migrate_chat(old_chat_id, new_chat_id)
-
-
-def __chat_settings__(chat_id, user_id):
-    return f"This chat has had it's rules set: `{bool(sql.get_rules(chat_id))}`"
-
-
-__help__ = """
- • `/rules`*:* get the rules for this chat.
-
-*Admins only:*
- • `/setrules <your rules here>`*:* set the rules for this chat.
- • `/clearrules`*:* clear the rules for this chat.
-"""
-
-__mod_name__ = "Rules 📛"
-
-GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
-SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
-RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.group)
-
-dispatcher.add_handler(GET_RULES_HANDLER)
-dispatcher.add_handler(SET_RULES_HANDLER)
-dispatcher.add_handler(RESET_RULES_HANDLER)
+    await event.reply("Successfully cleared rules for this chat !")
