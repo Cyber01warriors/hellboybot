393fc33b34b631322658c873b8eae9b5b7b203ff
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 09:14:55 +0530

    Update gtranslator.py

--- Mizuhararobot/modules/gtranslator.py
+++ Mizuhararobot/modules/gtranslator.py
@@ -137,6 +137,6 @@ TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "Translator ✅"
+__mod_name__ = "Translator 📄"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
