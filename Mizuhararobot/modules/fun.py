b05afce13d8f8cae0fbbb6c784453cb0ab1c80df
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 08:36:47 +0530

    Update fun.py

--- Mizuhararobot/modules/fun.py
+++ Mizuhararobot/modules/fun.py
@@ -233,7 +233,7 @@ __help__ = """
  • `/toss`*:* Tosses A coin
  • `/bluetext`*:* check urself :V
  • `/roll`*:* Roll a dice
- • `/rlg`*:* Join ears,nose,mouth and create an emo ;-;
+ • `/rlg`*:* Join ears, nose, mouth and create an emoji
  • `/shout <keyword>`*:* write anything you want to give loud shout
  • `/weebify <text>`*:* returns a weebified text
  • `/sanitize`*:* always use this before `/pat` or any contact
