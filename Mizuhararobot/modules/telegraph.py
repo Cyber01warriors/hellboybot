a42de73354928592d5288fa77aeec4d7e7d3a3ae
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-23 16:48:57 +0530

    Create telegraph.py

---
+++ Mizuhararobot/modules/telegraph.py
@@ -0,0 +1,38 @@
+import os
+import logging
+from PIL import Image
+from telethon import TelegramClient, events, Button
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+from telegraph import Telegraph, exceptions, upload_file
+from Mizuhararobot import telethn as bot
+
+@bot.on(events.NewMessage(pattern='/telegraph'))
+async def uploader(event):
+    TMP_DOWNLOAD_DIRECTORY = "./Downloads/"
+    if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TMP_DOWNLOAD_DIRECTORY)
+    pic = event.media
+    ok = await event.reply("`Downloading...`")
+    downloaded_file_name = await BotzHub.download_media(pic, TMP_DOWNLOAD_DIRECTORY)
+    if downloaded_file_name.endswith((".webp")):
+        await ok.edit("`Oh! It's a sticker...\nLemme convert it!!`")
+        resize_image(downloaded_file_name)
+    try:
+        media_urls = upload_file(downloaded_file_name)
+    except exceptions.TelegraphException as exc:
+        await ok.edit("**Error : **" + str(exc))
+        os.remove(downloaded_file_name)
+        return
+    else:
+        os.remove(downloaded_file_name)
+        await ok.edit("Uploaded to [Telegraph](https://telegra.ph{})\n\n~ @TheMizukiBot".format(media_urls[0]),
+                    link_preview=True,
+                    buttons=[
+                        Button.url("Link To File", url=f"https://telegra.ph{media_urls[0]}")
+                    ])
+
+def resize_image(image):
+    im = Image.open(image)
+    tmp = im.save(image, "PNG")
