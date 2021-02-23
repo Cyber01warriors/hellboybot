fdc7236012635a6f817a1206be5b4dceaea6f689
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-24 00:58:11 +0530

    Update playstore.py

--- Mizuhararobot/modules/playstore.py
+++ Mizuhararobot/modules/playstore.py
@@ -48,9 +48,9 @@ async def apk(e):
         app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
         app_details = "<a href='"+app_icon+"'>📲&#8203;</a>"
         app_details += " <b>"+app_name+"</b>"
-        app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
-        app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
-        app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
+        app_details += "\n\n<code>Developer:</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
+        app_details += "\n<code>Rating:</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
+        app_details += "\n<code>Features:</code> <a href='"+app_link+"'>View in Play Store</a>"
         app_details += "\n\n🔰 @TheMizukiBot 🔰"
         await e.reply(app_details, link_preview = True, parse_mode = 'HTML')
     except IndexError:
