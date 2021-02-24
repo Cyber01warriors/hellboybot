00af230a01cf728731332ca3099237ae60eaf46f
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-24 23:43:41 +0530

    Update github.py

--- Mizuki/modules/github.py
+++ Mizuki/modules/github.py
@@ -1,43 +1,113 @@
+import codecs
+import os
+import random
+import re
+from datetime import datetime
+from io import BytesIO
+from typing import Optional
+
 import requests
-from telethon import events
+import requests as r
+import wikipedia
+from bs4 import BeautifulSoup
+from requests import get, post
+from telegram import (
+    Chat,
+    ChatAction,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ParseMode,
+    ReplyKeyboardRemove,
+    TelegramError,
+    Update,
+)
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
+from telegram.ext.dispatcher import run_async
+from Mizuki.modules.disable import DisableAbleCommandHandler
+from Mizuki.modules.helper_funcs.alternate import send_action, typing_action
+from Mizuki.modules.helper_funcs.chat_status import user_admin
+from Mizuki.modules.helper_funcs.filters import CustomFilters
 
-from Mizuki import telethn as bot
+@run_async
+@typing_action
+def github(update, context):
+    message = update.effective_message
+    text = message.text[len("/git ") :]
+    usr = get(f"https://api.github.com/users/{text}").json()
+    if usr.get("login"):
+        text = f"*Username:* [{usr['login']}](https://github.com/{usr['login']})"
 
+        whitelist = [
+            "name",
+            "id",
+            "type",
+            "location",
+            "blog",
+            "bio",
+            "followers",
+            "following",
+            "hireable",
+            "public_gists",
+            "public_repos",
+            "email",
+            "company",
+            "updated_at",
+            "created_at",
+        ]
 
-@bot.on(events.NewMessage(pattern="/github"))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    url = "https://api.github.com/users/{}".format(input_str)
-    r = requests.get(url)
-    if r.status_code != 404:
-        b = r.json()
-        avatar_url = b["avatar_url"]
-        html_url = b["html_url"]
-        gh_type = b["type"]
-        name = b["name"]
-        company = b["company"]
-        blog = b["blog"]
-        location = b["location"]
-        bio = b["bio"]
-        created_at = b["created_at"]
-        await borg.send_file(
-            event.chat_id,
-            caption="""Name: [{}]({})
-Type: {}
-Company: {}
-Blog: {}
-Location: {}
-Bio: {}
-Profile Created: {}""".format(
-                name, html_url, gh_type, company, blog, location, bio, created_at
-            ),
-            file=avatar_url,
-            force_document=False,
-            allow_cache=False,
-            reply_to=event,
-        )
-        await event.delete()
+        difnames = {
+            "id": "Account ID",
+            "type": "Account type",
+            "created_at": "Account created at",
+            "updated_at": "Last updated",
+            "public_repos": "Public Repos",
+            "public_gists": "Public Gists",
+        }
+
+        goaway = [None, 0, "null", ""]
+
+        for x, y in usr.items():
+            if x in whitelist:
+                if x in difnames:
+                    x = difnames[x]
+                else:
+                    x = x.title()
+
+                if x == "Account created at" or x == "Last updated":
+                    y = datetime.strptime(y, "%Y-%m-%dT%H:%M:%SZ")
+
+                if y not in goaway:
+                    if x == "Blog":
+                        x = "Website"
+                        y = f"[Here!]({y})"
+                        text += "\n*{}:* {}".format(x, y)
+                    else:
+                        text += "\n*{}:* `{}`".format(x, y)
+        reply_text = text
     else:
-        await event.edit("`{}`: {}".format(input_str, r.text))
+        reply_text = "User not found. Make sure you entered valid username!"
+    message.reply_text(
+        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
+    )
+
+
+@run_async
+def repo(update, context):
+    context.args
+    message = update.effective_message
+    text = message.text[len("/repo ") :]
+    usr = get(f"https://api.github.com/users/{text}/repos?per_page=40").json()
+    reply_text = "*Repositorys*\n"
+    for i in range(len(usr)):
+        reply_text += f"[{usr[i]['name']}]({usr[i]['html_url']})\n"
+    message.reply_text(
+        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
+    )
+
+
+GITHUB_HANDLER = DisableAbleCommandHandler("git", github, admin_ok=True)
+REPO_HANDLER = DisableAbleCommandHandler("repo", repo, pass_args=True, admin_ok=True)
+
+dispatcher.add_handler(GITHUB_HANDLER)
+dispatcher.add_handler(REPO_HANDLER)
