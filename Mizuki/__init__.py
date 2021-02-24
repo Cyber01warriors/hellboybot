9eb010b4fc751664c64634c24c71a2b0e8d0eca2
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-24 22:09:56 +0530

    Update __init__.py

--- Mizuki/__init__.py
+++ Mizuki/__init__.py
@@ -2,10 +2,11 @@ import logging
 import os
 import sys
 import time
-
 import spamwatch
-import telegram.ext as tg
+
 from pyrogram import Client, errors
+
+import telegram.ext as tg
 from telethon import TelegramClient
 
 StartTime = time.time()
