51e25de608b2959073f40f89d97e1536954cd04d
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-05 15:41:23 +0530

    Create voicechat.py

---
+++ Mizuki/modules/voicechat.py
@@ -0,0 +1,13 @@
+__help__ = """
+Telegram Voice Chat Music Player ❤
+
+Commands:
+
+/play <yt link> - play song
+/skip or /next - skip song
+/pause - pause music streaming
+/end - stop music streaming
+
+© @TheMizukiBot 👸"""
+
+__mod_name__ = "Music Play 🎶"
