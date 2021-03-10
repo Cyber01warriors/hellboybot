f5680b96e0ddfe5b9da0ddfbe895b69b2d55cbf0
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-10 21:45:23 +0530

    Update Alarm.py

--- Mizuki/advanced/Alarm.py
+++ Mizuki/advanced/Alarm.py
@@ -143,10 +143,3 @@ async def tikclock(event):
             break
             return
         continue
-
-
-file_help = os.path.basename(__file__)
-file_help = file_help.replace(".py", "")
-file_helpo = file_help.replace("_", " ")
-
-CMD_HELP.update({file_helpo: [file_helpo, __help__]})
