09e334f79982de2328873dc5a9fdf26b9c4e6976
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-11 00:40:48 +0530

    Update advanced_search.py

--- Mizuki/modules/advanced_search.py
+++ Mizuki/modules/advanced_search.py
@@ -4,6 +4,14 @@ from youtube_search import YoutubeSearch
 
 from Mizuki import pbot as app
 from Mizuki.utils.errors import capture_err
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 # google
 
