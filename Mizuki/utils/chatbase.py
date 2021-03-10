b52b06e54de93b7ae3f202cbfce0c3077443f9f9
ImJanindu <janindumalshan259@gmail.com>
2021-03-10 17:22:10 +0000

    Mizuki Updated

--- Mizuki/utils/chatbase.py
+++ Mizuki/utils/chatbase.py
@@ -1,31 +1,33 @@
 # the logging things
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
 import os
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-  from Mizuki import CHAT_BASE_TOKEN
+    from Mizuki import CHAT_BASE_TOKEN
 else:
-  from Mizuki import CHAT_BASE_TOKEN
+    from Mizuki import CHAT_BASE_TOKEN
 
 # the Strings used for this "thing"
-from Mizuki.utils.anydl_trans import Translation
 
-from pyrogram import Client, filters
 
 # the Telegram trackings
 from chatbase import Message
 
 
 def TRChatBase(chat_id, message_text, intent):
-    msg = Message(api_key=CHAT_BASE_TOKEN,
-              platform="Telegram",
-              version="1.3",
-              user_id=chat_id,
-              message=message_text,
-              intent=intent)
-    Response = msg.send()
+    msg = Message(
+        api_key=CHAT_BASE_TOKEN,
+        platform="Telegram",
+        version="1.3",
+        user_id=chat_id,
+        message=message_text,
+        intent=intent,
+    )
+    msg.send()
