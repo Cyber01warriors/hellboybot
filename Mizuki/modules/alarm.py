432e4e5c44e2c21f6db22c7d9035093dad09cd6d
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-10 21:36:44 +0530

    Create alarm.py

---
+++ Mizuki/modules/alarm.py
@@ -0,0 +1,8 @@
+__help__ = """
+ • `/setalarm <(date) (time)|zone|reason>`*:* sets a alarm/reminder 
+**Syntax:** `/setalarm 01/01/2000 10:00:00 AM | America/New_York | breakfast`
+**NOTE:** 
+Please turn on notifications(PM/Group Chat) otherwise you will not get notification for the alarm !
+"""
+
+__mod_name__ = "Alarm ⏰"
