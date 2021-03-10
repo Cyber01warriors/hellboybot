d5d615c8d43ffa9021420ed3938abfcf23ef7dbe
ImJanindu <janindumalshan259@gmail.com>
2021-03-10 17:11:41 +0000

    Mizuki Updated

--- Mizuki/utils/conf.py
+++ Mizuki/utils/conf.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
@@ -22,7 +23,9 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/c80370e48458b2a7e1e92.jpg")
+    DEF_THUMB_NAIL_VID_S = os.environ.get(
+        "DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/c80370e48458b2a7e1e92.jpg"
+    )
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
