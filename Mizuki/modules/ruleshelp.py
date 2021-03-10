2964d8cf4c2ec0578bae2aa8fdedca2c28116c79
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-10 17:56:13 +0530

    Create ruleshelp.py

---
+++ Mizuki/modules/ruleshelp.py
@@ -0,0 +1,9 @@
+__help__ = """
+ • `/rules`*:* get the rules for this chat.
+
+*Admins only:*
+ • `/setrules <your rules here>`*:* set the rules for this chat.
+ • `/clearrules`*:* clear the rules for this chat.
+"""
+
+__mod_name__ = "Rules 📛"
