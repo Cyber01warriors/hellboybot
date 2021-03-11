991303504e355507922a151ab06e8f650c75eafe
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-11 23:53:45 +0530

    Create moretools.py

---
+++ Mizuki/modules/moretools.py
@@ -0,0 +1,13 @@
+__help__ = """
+*Text To Speech:*
+ ⦁ `/tts <text>`*:* convert text to speech.
+
+*Zipper:*
+ ⦁ `/zip`*:* reply to a telegram file to compress it in .zip format.
+ ⦁ `/unzip`*:* reply to a telegram file to decompress it from the .zip format.
+
+*Zombies:*
+ ⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results.
+"""
+
+__mod_name__ = "More Tools 💡"
