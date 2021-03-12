029e2603c9af5c60d2a56a9ded6ff7e43636b57a
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-12 08:18:58 +0530

    Create exclusive.py

---
+++ Mizuki/modules/exclusive.py
@@ -0,0 +1,15 @@
+__help__ = """
+*✖ URL Upload ✖*
+ ⦁ `/up`*:* reply to a direct download link to upload it to telegram as files.
+
+*✖ File To Link ✖*
+ ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link.
+ ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link.
+ ⦁ `/getlink`*:* reply to telegram file to get direct download link quickly.
+
+*✖ Screenshot Gen ✖*
+ ⦁ `/genss`*:* reply to a video to get some screen shots of it.
+
+© @TheMizukiBot 👸"""
+
+__mod_name__ = "Exclusive 🧩"
