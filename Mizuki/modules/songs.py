f3be89d825a6bd1ef80cf126342895e04dcf81dd
ImJanindu <janindumalshan259@gmail.com>
2021-03-13 18:44:11 +0000

    Mizuki Updated

--- Mizuki/modules/songs.py
+++ Mizuki/modules/songs.py
@@ -4,8 +4,6 @@ import json
 import os
 import time
 
-from Mizuki.events import register
-from Mizuki import progress
 from telethon.tl.types import DocumentAttributeAudio
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import (
@@ -18,6 +16,9 @@ from youtube_dl.utils import (
     UnavailableVideoError,
     XAttrMetadataError,
 )
+
+from Mizuki import progress
+from Mizuki.events import register
 
 try:
 
