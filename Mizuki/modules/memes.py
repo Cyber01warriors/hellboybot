5d562a5950aaf434f233618aa53145ad6fab255c
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-15 21:42:44 +0530

    Create memes.py

---
+++ Mizuki/modules/memes.py
@@ -0,0 +1,19 @@
+import aiohttp
+from aiogram import types
+from Mizuki import pbot
+
+@pbot.on_message(filters.command("memes"))
+async def memes(client, message):
+    async with aiohttp.ClientSession() as ses:
+        async with ses.get(
+            'https://some-random-api.ml/meme'
+        ) as resp:
+            r = await resp.json()
+            await message.reply_photo(r['image'], caption=r['caption'])
+
+__help__ = """
+<b>Mizuki gives you memes randomly gained through meme API</b>
+- /memes: get very interesting and funny memes randomly.
+"""
+
+__mod_name__ = "Memes"
