ac60f6cf6bdba40867f0f242f577ec47dca070c6
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-15 20:29:56 +0530

    Update polling.py

--- Mizuki/modules/polling.py
+++ Mizuki/modules/polling.py
@@ -403,12 +403,12 @@ Here is how you can do it:
  ▪️ <True@optionnumber/False>(1) - quiz mode, you must state the correct answer with `@` eg: `True@1` or `True@2`
  ▪️ <True/False>(2) - public votes
  ▪️ <True/False>(3) - multiple choice
-**Syntax:**
+*Syntax:*
 > `/poll <poll-id> <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
-**Examples:**
+*Examples:*
 - `/poll 12345 | am i cool? | False False False yes no`
 - `/poll 12345 | am i cool? | True@1 False False yes no`
-**To stop a poll**
+*To stop a poll*
 Reply to the poll with `/stoppoll <poll-id>` to stop the poll
 *NOTE:*
 • If you have forgotten your poll id or deleted the poll so that you can't stop the previous poll type `/forgotpollid`, this will reset the poll id, you will have no access to the previous poll!
