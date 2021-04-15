be06b69a39b46f0692e067f0c77f08a96b539bb7
ImJanindu <janindumalshan259@gmail.com>
2021-04-15 06:03:15 +0000

    Mizuki Updated

--- Mizuki/modules/heroku.py
+++ Mizuki/modules/heroku.py
@@ -1,11 +1,14 @@
 import os
 import sys
-from Mizuki.function.heroku_helper import HerokuHelper
+
 import heroku3
-import requests
-from Mizuki.events import register
+
 from Mizuki import HEROKU_API_KEY, HEROKU_APP_NAME, OWNER_ID
+from Mizuki.events import register
+from Mizuki.function.heroku_helper import HerokuHelper
+
 Heroku = heroku3.from_key(HEROKU_API_KEY)
+
 
 @register(pattern="^/restart$")
 async def _(event):
