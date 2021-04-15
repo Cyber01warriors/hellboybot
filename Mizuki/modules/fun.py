5acb449eb4dc162c1133d4f8ae8cb979fddf93e5
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-15 18:07:43 +0530

    Update fun.py

--- Mizuki/modules/fun.py
+++ Mizuki/modules/fun.py
@@ -344,7 +344,7 @@ dispatcher.add_handler(RLG_HANDLER)
 dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "Fun 😜"
+__mod_name__ = "Fun"
 __command_list__ = [
     "runs",
     "slap",
