bc3c1bf5ca50d6566ba1390d2d92d34466e7893c
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-15 21:59:18 +0530

    Update moretools.py

--- Mizuki/modules/moretools.py
+++ Mizuki/modules/moretools.py
@@ -1,25 +1,28 @@
 __help__ = """
 *Text To Speech:*
- ⦁ `/tts <text>`*:* convert text to speech.
+⦁ `/tts <text>`*:* convert text to speech.
 
 *Zipper:*
- ⦁ `/zip`*:* reply to a telegram file to compress it in .zip format.
- ⦁ `/unzip`*:* reply to a telegram file to decompress it from the .zip format.
+⦁ `/zip`*:* reply to a telegram file to compress it in .zip format.
+⦁ `/unzip`*:* reply to a telegram file to decompress it from the .zip format.
 
 *Zombies:*
- ⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results.
+⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results.
 
 *Stickers:*
- ⦁ `/stickerid`*:* reply to a sticker to me to tell you its file ID.
- ⦁ `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
- ⦁ `/kang`*:* reply to a sticker to add it to your pack.
- ⦁ `/stickers`*:* Find stickers for given term on combot sticker catalogue.
+⦁ `/stickerid`*:* reply to a sticker to me to tell you its file ID.
+⦁ `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
+⦁ `/kang`*:* reply to a sticker to add it to your pack.
+⦁ `/stickers`*:* Find stickers for given term on combot sticker catalogue.
+
+*Cricket:*
+• /cs*:* get latest live cricket matches scores from cricinfo.
 
 *Time:*
- ⦁ `/time <query>`*:* gives information about a timezone.
+⦁ `/time <query>`*:* gives information about a timezone.
 
 *Available queries:* Country Code/Country Name/Timezone Name
-• 🕒 [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+⦁ 🕒 [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
 """
 
 __mod_name__ = "More Tools"
