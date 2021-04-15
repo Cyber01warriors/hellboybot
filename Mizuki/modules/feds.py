75a51c5eda3cb3d9bfe9e1250e7cfdafa412d232
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-15 20:30:46 +0530

    Update feds.py

--- Mizuki/modules/feds.py
+++ Mizuki/modules/feds.py
@@ -2390,7 +2390,7 @@ __help__ = """
 Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
 But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
 *No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
-You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
+You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.
 
 *Commands:*
 Feds are now divided into 3 sections for your ease. 
