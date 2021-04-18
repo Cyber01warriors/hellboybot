d31b6fb7cdc10bfc3151731278764b5f69eb2332
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-18 23:36:15 +0530

    Update memes.py

--- Mizuki/modules/memes.py
+++ Mizuki/modules/memes.py
@@ -7,9 +7,9 @@ from Mizuki import pbot
 @pbot.on_message(filters.command("memes"))
 async def memes(client, message):
     async with aiohttp.ClientSession() as ses:
-        async with ses.get("https://some-random-api.ml/meme") as resp:
+        async with ses.get("https://meme-api.herokuapp.com/gimme/wholesomememes") as resp:
             r = await resp.json()
-            await message.reply_photo(r["image"], caption=r["caption"])
+            await message.reply_photo(r["url"], caption=r["title"])
 
 
 __help__ = """
