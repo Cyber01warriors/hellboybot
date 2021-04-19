595a4813da56eed18eb648afe2aec24f5e3a17de
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-19 11:32:37 +0530

    Create admincheck.py

---
+++ Mizuki/utils/admincheck.py
@@ -0,0 +1,47 @@
+from typing import List
+from pyrogram.types import Chat, User
+from typing import List, Dict, Union
+from typing import Callable
+from Mizuki import OWNER_ID
+
+admins: Dict[int, List[int]] = {}
+
+
+def set(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+
+def get(chat_id: int) -> Union[List[int], bool]:
+    if chat_id in admins:
+        return admins[chat_id]
+
+    return False
+
+async def get_administrators(chat: Chat) -> List[User]:
+    get = admins.get(chat.id)
+
+    if get:
+        return get
+    else:
+        administrators = await chat.get_members(filter="administrators")
+        to_set = []
+
+        for administrator in administrators:
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
+
+        admins.set(chat.id, to_set)
+        return await get_administrators(chat)
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in OWNER_ID:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
+    return decorator
