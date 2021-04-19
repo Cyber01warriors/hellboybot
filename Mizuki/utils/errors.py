e0c9bb0d225939242b3ca473b6378b2998897dce
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-19 19:20:38 +0530

    Update errors.py

--- Mizuki/utils/errors.py
+++ Mizuki/utils/errors.py
@@ -1,9 +1,9 @@
 import sys
 import traceback
 from functools import wraps
-
-from Mizuki import EVENT_LOGS
 from Mizuki import pbot as app
+from Mizuki import EVENT_LOGS as LOG_GROUP_ID
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 
 def split_limits(text):
@@ -11,7 +11,7 @@ def split_limits(text):
         return [text]
 
     lines = text.splitlines(True)
-    small_msg = ""
+    small_msg = ''
     result = []
     for line in lines:
         if len(small_msg) + len(line) < 2048:
@@ -30,23 +30,26 @@ def capture_err(func):
     async def capture(client, message, *args, **kwargs):
         try:
             return await func(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await app.leave_chat(message.chat.id)
+            return
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
+                etype=exc_type, value=exc_obj, tb=exc_tb,
             )
             error_feedback = split_limits(
-                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
+                '**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n'.format(
                     0 if not message.from_user else message.from_user.id,
                     0 if not message.chat else message.chat.id,
                     message.text or message.caption,
-                    "".join(errors),
+                    ''.join(errors),
                 ),
             )
             for x in error_feedback:
-                await app.send_message(EVENT_LOGS, x)
+                await app.send_message(
+                    LOG_GROUP_ID,
+                    x
+                )
             raise err
-
     return capture
