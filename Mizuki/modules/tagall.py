c25bfbdc438a5cd55fb675d9cb03d974b786f91e
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-20 08:45:51 +0530

    Create tagall.py

---
+++ Mizuki/modules/tagall.py
@@ -0,0 +1,34 @@
+import asyncio
+from telethon.tl.types import ChannelParticipantsAdmins
+from Mizuki.events import register as mizuki
+import telethon
+from telethon import events
+
+
+@mizuki(pattern="^/tagall")
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "Hello"
+    chat = await event.get_input_chat()
+    async for x in mizuki.iter_participants(chat, 100):
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
+    await event.reply(mentions)
+    await event.delete()
+
+
+@mizuki(pattern="^/administrator")
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "Administrators: "
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
