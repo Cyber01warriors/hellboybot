f696c75ab06058fcc3c4290127fb9e9afdf3eaf0
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-20 12:54:46 +0530

    Update exclusive.py

--- Mizuki/modules/exclusive.py
+++ Mizuki/modules/exclusive.py
@@ -4,16 +4,6 @@ __help__ = """
 
 *𝐅𝐢𝐥𝐞 𝐓𝐨 𝐋𝐢𝐧𝐤*
  ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link.
- ⦁ `/getlink`*:* reply to telegram file to get direct download link quickly.
-
-*𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭 𝐆𝐞𝐧*
- ⦁ `/genss`*:* reply to a video to get some screen shots of it.
-
-*𝐅𝐢𝐥𝐞 𝐑𝐞𝐧𝐚𝐦𝐞𝐫*
- ⦁ `/rename newfilename.extension`*:* rename any telegram media file.
-
-*𝐂𝐨𝐧𝐯𝐞𝐫𝐭 𝐓𝐨 𝐕𝐢𝐝𝐞𝐨*
- ⦁ `/c2v`*:* reply to video file to convert it as streamable video.
 
 *𝐕𝐢𝐫𝐮𝐬 𝐒𝐜𝐚𝐧*
  ⦁ `/scanit`*:* Scan a file for virus (MAX SIZE = 3MB)
